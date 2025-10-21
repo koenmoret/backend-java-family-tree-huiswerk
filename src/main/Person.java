d4ce74a5feb5408ac0d1b7c0975638ea23a945c5
koenmoret <koen@moret.info>
2025-10-21 12:37:34 +0200

    create class Person

---
+++ src/main/Person.java
@@ -0,0 +1,50 @@
+package main;
+
+public class Person {
+
+    private String name;
+    private String middleName;
+    private String lastName;
+    private String sex;
+    private int age;
+
+    public Person(String name, String middleName, String lastName, String sex, int age) {
+        this.name = name;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
