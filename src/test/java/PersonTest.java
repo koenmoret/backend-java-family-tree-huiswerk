e01908e0a1e3f944c05e9e1fa1872fa538a86bc5
koenmoret <koen@moret.info>
2025-10-22 13:19:53 +0200

    import tests from class Person

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,4 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PersonTest {
 
@@ -12,5 +15,83 @@ public class PersonTest {
     @BeforeEach
     void CreatePerson1() {
         person1 = new Person(person1Name, person1LastName, person1sex, person1Age);
+    }
+
+    @Test
+    void getName() {
+        String name = person1.getName();
+        assertEquals(person1Name, name); //gelijk aan de verwachte waarde
+    }
+
+    @Test
+    void getMiddleName() {
+    }
+
+    @Test
+    void getLastName() {
+    }
+
+    @Test
+    void getSex() {
+    }
+
+    @Test
+    void getAge() {
+    }
+
+    @Test
+    void getMother() {
+    }
+
+    @Test
+    void getFather() {
+    }
+
+    @Test
+    void getChildren() {
+    }
+
+    @Test
+    void getPets() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void setMiddleName() {
+    }
+
+    @Test
+    void setLastName() {
+    }
+
+    @Test
+    void setSex() {
+    }
+
+    @Test
+    void setAge() {
+    }
+
+    @Test
+    void addParents() {
+    }
+
+    @Test
+    void addChild() {
+    }
+
+    @Test
+    void addPet() {
+    }
+
+    @Test
+    void addSibling() {
+    }
+
+    @Test
+    void getGrandChildren() {
     }
 }
